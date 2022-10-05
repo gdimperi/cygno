0f8359ebbe2f875d710ffd0f06f95cc293f9fabb
Giovanni Mazzitelli <giovanni.mazzitelli@lnf.infn.it>
2022-10-05 18:46:42 +0200

    boto3sts lngs workaround

--- setup.py
+++ setup.py
@@ -31,6 +31,6 @@ setup(
     'tqdm'
     ],
     dependency_links=[
-        'https://github.com/DODAS-TS/boto3sts.git#egg=boto3sts_alt-0.0.8'  # Link with version at the end
+        'git+https://github.com/DODAS-TS/boto3sts.git#egg=boto3sts_alt-0.0.8'  # Link with version at the end
     ]
 )
